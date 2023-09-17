12c3764faf5d13b9298347603eb8749bd178c738
HugoLmb <lamberthug@eisti.eu>
2023-09-17 11:09:15 +0200

    Reprise du code existant, amélioration de la structure : Graphics, Maths

--- BM_def.py
+++ BM_def.py
@@ -10,16 +10,12 @@ Wt2 = Wt1 + g2√Delta_t
 WtN = WtN−1 + gN√Delta_t
 '''
 
-
 import numpy as np
+from Maths import norm
 
-def norm():
-    rand = np.random.randn()
-    return rand
 def BM(N, T):
     delta_t = T/N
     W_t = [0]
-    time = np.linspace(0, T, N+1)
     for i in range(N):
         W_t.append(W_t[i] + norm()*np.sqrt(delta_t))
-    return [time, W_t]
+    return W_t
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import BM_def
+import numpy as np
+from Graphics import plot_2d
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    time, W_t = BM_def.BM(100, 1)
-    plt.plot(time, W_t)
-    plt.title("Simulation du mouvement brownien")
+    N = 100
+    T = 1
+    Nmc = 1000
+    time = np.linspace(0, T, N+1)
+    title = f'SImulation de {Nmc} mouvement brownien'
+    x_axis = 'Temps'
+    y_axis = 'Valeur'
+    for i in range(Nmc):
+        W_t = BM_def.BM(N, T)
+        plot_2d(time, W_t, title, x_axis, y_axis, False)
     plt.show()
+
+
