8937717337934dde262710e746212e6aa95f8cf7
HugoLmb <lamberthug@eisti.eu>
2023-12-10 21:30:05 +0100

    update : mu & sigma computed for bs dynamic of a stock. todo : review the graph plotting function. (for df?)

--- Actif_stoch_BS.py
+++ Actif_stoch_BS.py
@@ -10,9 +10,12 @@ sigma est la volatilité de l'action
 la solution de l'EDS est donnée par :
 S(t) = S(0)*exp((mu-0.5sigma**2)$t + sigmaWt)
 '''
+from matplotlib import pyplot as plt
 
 from BM_def import BM
 import numpy as np
+import yfinance as yf
+
 from Maths import norm_
 def simu_actif(init, N, t, T, mu, sigma):
     St = [init]
@@ -32,3 +35,18 @@ def option_eu_mc(St, t, T, K, Nmc):
         prix_option += payoff_call_eu(prix_actif[-1], K)
     prix_option = prix_option/Nmc
     return prix_option
+def simu_stock_vs_hist(ticker_symbol):
+    lengend = ['Market', 'Simulated']
+    stock_data = yf.download(ticker_symbol, start='2022-01-01', end='2023-12-08')
+    stock_data.Close.plot()
+
+    stock_data['variation_daily'] = stock_data['Close'].pct_change()*100
+    mu = (stock_data['variation_daily'].resample('Y').mean()).mean()
+    sigma = stock_data['variation_daily'].std()/10
+    St = stock_data['Close'].iloc[0]
+    N = stock_data['Close'].size - 1
+    x = stock_data.index.to_list()
+    y = simu_actif(St, N, 0, N/365.6, mu, sigma)
+    plt.plot(x, y)
+    plt.legend(lengend)
+    plt.show()
--- future_prices.py
+++ future_prices.py
@@ -1,21 +1,32 @@
+from statistics import mean
+
 import yfinance as yf
 from datetime import datetime, timedelta
 import numpy as np
 from matplotlib import pyplot as plt
-
-from Actif_stoch_BS import simu_actif
-def get_future_prices(ticker_symbol, maturities):
-
-    stock_data = yf.download(ticker_symbol, start='2022-01-01', end='2023-12-08')
-    plt.plot((stock_data.Close)
+from Actif_stoch_BS import simu_actif, simu_stock_vs_hist
+from interest_rates import Tresury_bond_5years
 
 
 def future_price(St, t, T, r):
+    Future_price = St*np.exp(r*(T-t))
+    return Future_price
+def investor_future_price(ticker_symbol, t, T, r):
     epsilone = 0 #corresponds to the systemic risk
     k = r + epsilone
-    ST_expected = simu_actif(St, 100, t, T, 0.1, 0.3)
+    stock_data = yf.download(ticker_symbol, start='2022-01-01', end='2023-12-08')
+    St = stock_data['Close'].iloc[0]
+    stock_data['variation_daily'] = stock_data['Close'].pct_change()*100
+    mu = (stock_data['variation_daily'].resample('Y').mean()).mean()
+    sigma = (stock_data['variation_daily'].resample('Y').std()).mean() / 10
+    simulations = []
+    Nmc = 1000
+    for i in range(Nmc):
+        simulations.append(simu_actif(St, 100, 0, T, mu, sigma)[-1])
+    ST_expected = mean(simulations)
     Future_price = ST_expected*np.exp((r-k)*(T-t))
-    return
+    return Future_price
 
 if __name__ == '__main__':
-    get_future_prices('AAPL', 'maturities')
+
+    print(investor_future_price('AAPL', 0, 2, Tresury_bond_5years))
