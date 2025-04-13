28644928cedf3f54a7880b1e806f2738cfc2e8ba
HugoLmb <lamberthug@eisti.eu>
2025-04-13 20:35:16 +0200

    requirements.txt updated.

--- Asset_Modeling/Energy_Modeling/ShortTerm_Power/DA_AuctionFR.py
+++ Asset_Modeling/Energy_Modeling/ShortTerm_Power/DA_AuctionFR.py
@@ -3,11 +3,11 @@ import os
 import pandas as pd
 
 def updatefileDA_Auction():
-    os.chdir('../../../API')
-    data = pd.read_excel("../Data/DA_PriceFR.xlsx")
+    ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../'))
+    data_path = os.path.join(ROOT_DIR, 'Data', 'DA_PriceFR.xlsx')
+    data = pd.read_excel(data_path)
     df_da_prices = getAPIdata(APIname="Wholesale Market")
     df_da_prices['date'] = pd.to_datetime(df_da_prices['date'], errors='coerce')
-
     df_da_prices['date'] = pd.to_datetime(
         df_da_prices['date'].astype(str) + ' ' + df_da_prices['time'].astype(str),
         format='%Y-%m-%d %H:%M:%S'
@@ -18,10 +18,8 @@ def updatefileDA_Auction():
     data = data.drop_duplicates(subset=['date'], keep='last')
     data.reset_index(drop=True, inplace=True)
 
-    with pd.ExcelWriter("../Data/DA_PriceFR.xlsx", engine='openpyxl') as writer:
+    with pd.ExcelWriter(data_path, engine='openpyxl') as writer:
         data.to_excel(writer, index=False)
-    return
-
 
 if __name__ == '__main__':
     updatefileDA_Auction()
