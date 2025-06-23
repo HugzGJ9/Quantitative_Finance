b07dc3de64acf86db733f07c28c5391fc3672d04
HugoLmb <lamberthug@eisti.eu>
2025-06-23 12:31:45 +0200

    Respy codes removed.

--- API/ENTSOE/client.py
+++
@@ -1,4 +0,0 @@
-from entsoe import EntsoePandasClient
-from Keypass.key_pass import API_KEY_ENTSOE
-CLIENT = EntsoePandasClient(api_key=API_KEY_ENTSOE)
-
--- Asset_Modeling/Energy_Modeling/ShortTerm_Power/dailySaveDB.py
+++
@@ -1,5 +0,0 @@
-from API.SUPABASE.save import saveDailyGeneration, saveDailyAuction
-
-if __name__ == '__main__':
-    saveDailyAuction()
-    saveDailyGeneration()
